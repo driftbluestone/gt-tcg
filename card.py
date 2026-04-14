b5ed48d5cdc42b27a06f9780e7a68a0fb3b80040
driftbluestone <driftbluestone@gmail.com>
2026-04-13 18:58:38 -0700

    scale can now be a float

--- card.py
+++ card.py
@@ -21,7 +21,7 @@ class BaseCard():
             field["position"] = [x + y for x, y in zip(field["position"], offset)]
             self.fields[field["name"]] = field["position"]
 
-    def set_field(self, name: str, field: Image.Image | str, *, scale: int | Literal["consistent"] = 1, padding: tuple[int, int] = None, color: tuple[int, int, int] = None) -> bool:
+    def set_field(self, name: str, field: Image.Image | str, *, scale: float | Literal["consistent"] = 1, padding: tuple[int, int] = None, color: tuple[int, int, int] = None) -> bool:
         """
         Sets a card field to be an image or text.
         Setting scale to "consistent" means that it will always be the same size regardless of image scale
@@ -35,17 +35,17 @@ class BaseCard():
         if scale == "scale":
             scale = self.scale
         if scale != 1:
-            field = self.image.resize((scale * field.width, scale * field.height), resample = Image.BOX)
+            field = self.image.resize((int(scale * field.width), int(scale * field.height)), resample = Image.BOX)
         self.image.paste(field, tuple([x + y for x, y in zip(self.fields[name], padding)]), field)
         return True
     
-    def resize(self, scale: int):
+    def resize(self, scale: float):
         """
         Resize the image while keeping field positions consistent
         """
         for k, v in self.fields.items():
-            self.fields[k] = [x * scale for x in v]
-        self.image = self.image.resize((scale * self.image.width, scale * self.image.height), resample = Image.BOX)
+            self.fields[k] = [int(x * scale) for x in v]
+        self.image = self.image.resize((int(scale * self.image.width), int(scale * self.image.height)), resample = Image.BOX)
         self.scale *= scale
     
 class Card(BaseCard):
@@ -59,9 +59,9 @@ im = Card({"base":"test_card"})
 im.set_field("title", "MissingNo.", padding=(0, 1), color=(34, 51, 255))
 im.set_field("move_1", "Confuse!", padding=(0, 0), color=(0, 0, 0))
 im.set_field("move_2", "idk what to write here", padding=(0, 0), color=(0, 0, 0))
-im.resize(2)
-im.set_field("move_description_1", "dn dn dn\nthis field\nfits 5 lines\nof half scaled text!\nisn't that fun", padding=(5, -5), color=(0, 0, 0))
-im.set_field("move_description_2", "age release when\nim waiting!!", padding=(5, -5), color=(0, 0, 0))
+im.resize(0.5)
+im.set_field("move_description_1", "lmao scaling it\ndown is so\ngoofy", padding=(5, -5), color=(0, 0, 0))
+# im.set_field("move_description_2", ".", padding=(5, -5), color=(0, 0, 0))
 im.resize(4)
 
 
